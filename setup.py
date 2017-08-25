15530159a4673f91bb9a81363b4f4963e728c3fa
晏立 <yanli@yellfun.com>
2017-08-25 22:50:19 +0800

    save

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["jwt", 'appdirs', 'packaging.version']}
+build_exe_options = {"packages": ["jwt", 'appdirs', 'packaging']}
 
 setup(
     name = 'pack',
