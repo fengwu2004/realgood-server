9dbfc4f66b7ad06b934c2e6fcd9c8b5849a8d4a8
晏立 <yanli@yellfun.com>
2017-09-04 22:34:12 +0800

    save

---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+import sys
+
+build_exe_options = {"packages": ["jwt", 'appdirs', 'packaging', 'tornado'], 'path':sys.path + [sys.path[0] + '/analyse', sys.path[0] + '/webserver', sys.path[0] + '/data', sys.path[0] + '/stock']}
+
+print(build_exe_options['path'])
+
+setup(
+    name = 'pack',
+    version = '0.1',
+    options = {'build_exe': build_exe_options},
+    executables = [Executable('main.py', base = None)]
+)
