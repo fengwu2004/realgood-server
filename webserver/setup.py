dc1f82b228dc28872776ec62fb091b6558b1af92
晏立 <yanli@yellfun.com>
2017-09-04 21:52:09 +0800

    save

--- webserver/setup.py
+++ webserver/setup.py
@@ -6,5 +6,5 @@ setup(
     name = 'pack',
     version = '0.1',
     options = {'build_exe': build_exe_options},
-    executables = [Executable('main.py', base = None)]
+    executables = [Executable('webserver/main.py', base = None)]
 )
